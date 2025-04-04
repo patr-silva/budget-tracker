9c7dfc31e2bb08afb125ffb46515a40fed6d9d07
Patrícia Silva <98959396+patr-silva@users.noreply.github.com>
2025-04-04 15:24:14 +0100

    transaction service created + load and save transactions to localStorage methods implemented

---
+++ src/app/services/transaction.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Transaction } from '../models/transaction.model';
+import { v4 as uuid } from 'uuid';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionService {
+  private transactions: Transaction[] = [];
+  private storageKey: string = 'transactions';
+
+  constructor() {}
+
+  private loadTransactions(): void {
+    const storedTransactions = localStorage.getItem(this.storageKey);
+    this.transactions = storedTransactions
+      ? JSON.parse(storedTransactions)
+      : [];
+  }
+
+  private saveTransactions(): void {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.transactions));
+  }
+
+  addTransaction(transaction: Transaction) {
+    const newTransaction: Transaction = {
+      ...transaction,
+      id: uuid(),
+      date: new Date(transaction.date),
+    };
+
+    this.transactions.push(newTransaction);
+    this.saveTransactions();
+
+    console.log(newTransaction);
+  }
+
+  deleteTransaction(id: number) {}
+  updateTransaction(id: number) {}
+  getBalance() {}
+}
